42aec2aa89e6f534a17a93f5e2137713c3d694b1
Danski <danieljsimons@hotmail.com>
2014-04-09 21:35:12 +1000

    Initial Database tables

---
+++ database/ddl/CREATE_DATABASE.sql
@@ -0,0 +1,26 @@
+﻿create user advisorbook_owner with password 'advisorbook_ownerdev';
+
+CREATE TABLESPACE ab_default
+  OWNER advisorbook_owner
+  LOCATION 'C:\\data\\postgres';
+COMMENT ON TABLESPACE ab_default
+  IS 'Advisor Book Default';
+
+CREATE DATABASE advisorbook
+  WITH OWNER = advisorbook_owner
+       ENCODING = 'UTF8'
+       TABLESPACE = ab_default
+       CONNECTION LIMIT = 2;
+
+COMMENT ON DATABASE postgres
+  IS 'default administrative connection database';
+
+
+CREATE SCHEMA "AB_CORE"
+  AUTHORIZATION advisorbook_owner;
+
+COMMENT ON SCHEMA "AB_CORE"
+  IS 'Advisor Book Core schema';
+
+
+  
